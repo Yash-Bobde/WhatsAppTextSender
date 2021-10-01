ac1e38342a5251fe69ac7be10c04fd19c5b40732
Yash Bobde <ymbobde99@gmail.com>
2021-10-01 10:55:56 +0530

    Update ok.py

--- ok.py
+++ ok.py
@@ -1,2 +1,2 @@
 import pywhatkit
-pywhatkit.sendwhatmsg('+91(write the number on which you want to send here)','what is the message type here' , 12, 16)
+pywhatkit.sendwhatmsg('+91(write the number on which you want to send here)','what is the message type here' , enter time_in_hour, enter time_in_min)
